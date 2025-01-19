5e70d36c5bed0437a9955f6e5124006d69cb9cde
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-01-19 11:20:31 +0530

    JSON and DART Conversion

--- lib/models/expence_model.dart
+++ lib/models/expence_model.dart
@@ -7,8 +7,9 @@ enum ExpenceCategory {
   shopping,
   subscriptions,
 }
+
 //Images
-final Map<ExpenceCategory, String> expenceCategoriesImages ={
+final Map<ExpenceCategory, String> expenceCategoriesImages = {
   ExpenceCategory.food: "assets/images/fastfood.png",
   ExpenceCategory.transport: "assets/images/bus.png",
   ExpenceCategory.health: "assets/images/care.png",
@@ -43,4 +44,29 @@ class Expence {
     required this.time,
     required this.description,
   });
+  //Convert to Json (Serialization)
+  Map<String, dynamic> toJSON() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category.index,
+      'date': date.toIso8601String(),
+      'time': time.toIso8601String(),
+      'description': description,
+    };
+  }
+
+  //Convert to Object Again (Deserialization)
+  factory Expence.formJSON(Map<String, dynamic> json) {
+    return Expence(
+        id: json['id'],
+        title: json['title'],
+        amount: json['amount'],
+        category: ExpenceCategory.values[json['category']],
+        date: DateTime.parse(json['date']),
+        time: DateTime.parse(json['time']),
+        description: json['description'],
+        );
+  }
 }
--- lib/models/income_model.dart
+++ lib/models/income_model.dart
@@ -21,9 +21,8 @@ final Map<IncomeCategory, Color> incomeCategoryColor = {
   IncomeCategory.passive: const Color.fromARGB(221, 255, 0, 200),
   IncomeCategory.salery: const Color.fromARGB(255, 238, 255, 0),
   IncomeCategory.sales: const Color.fromARGB(255, 23, 192, 1),
-
-
 };
+
 class Income {
   final int id;
   final String title;
@@ -42,4 +41,30 @@ class Income {
     required this.time,
     required this.description,
   });
+
+  //Convert to Json (Serialization)
+  Map<String, dynamic> toJSON() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category.index,
+      'date': date.toIso8601String(),
+      'time': time.toIso8601String(),
+      'description': description,
+    };
+  }
+
+  //Convert to Object Again (Deserialization)
+  factory Income.formJSON(Map<String, dynamic> json) {
+    return Income(
+      id: json['id'],
+      title: json['title'],
+      amount: json['amount'],
+      category: IncomeCategory.values[json['category']],
+      date: DateTime.parse(json['date']),
+      time: DateTime.parse(json['time']),
+      description: json['description'],
+    );
+  }
 }
